e116e81465038cf2183f79d2a2402d25d043bd3d
Evan Ó Catháin <Evan.Keane@gmail.com>
2020-03-30 12:01:29 +0100

    Add L^2 norm option
    Add L^2 norm option + some misc tidying up.

--- script.csh
+++ script.csh
@@ -3,11 +3,11 @@
 rm output*ascii pulses*pls
 set destroy = ~/GIT/destroy_gutted/destroy
 
-foreach option ( 1 2 3 )
+foreach option ( 1 2 3 4 )
 
     foreach height ( 40 ) 
 
-	python test.py -option $option -height $height -width 1
+	python test.py -option $option -height $height -width 8
 	$destroy -ascii -n 8192 -box 2 -nsmax 8 "out"$option".ascii"
 	mv pulses.pls "pulses"$option".pls"
 
--- test.py
+++ test.py
@@ -21,11 +21,6 @@ option = args.option         # 1 - square pulses, 2 - Gaussian pulses, equal are
 height = args.height
 Nsamps = args.nsamps
 period_bins = args.period
-#dt = 0.001                        # in seconds, say
-#Tobs = Nsamps*dt
-#df = 1.0/Tobs
-#period = period_bins*dt
-#nperiods = int(Tobs/period)
 nperiods = int(Nsamps/period_bins)
 
 seed = args.seed
@@ -33,35 +28,40 @@ if (seed != 0):
     np.random.seed(seed)
 noise = normal(0.0,1.0,Nsamps)
 A = 1.0
-#width = period*0.016               # Set the W/P
-#width_bins = int(width/dt)
 width_bins = int(args.width)
-#sigma_bins = sigma/dt
-offset_bins = period_bins*0.5 + 0.5
+offset_bins = int(period_bins*0.5)
 
 fac = m.sqrt(np.pi/np.log(2.0))*0.5 # = 1.0644670194 ... this ensures area under pulse is the same boxcar vs Gaussian Option2
 
-prof = np.zeros(period_bins)
-if (option == 1):
+prof   = np.zeros(period_bins)
+profsq = np.zeros(period_bins)
+if (option == 1):                     # This is a boxcar
     outf = open("out1.ascii", "w")
-    prof = np.hstack((np.zeros(period_bins-width_bins),np.ones(width_bins)))
-if (option == 2):
+    prof = np.hstack((np.zeros(period_bins-offset_bins-width_bins/2),np.ones(width_bins),np.zeros(offset_bins+width_bins-width_bins/2)))
+if (option == 2):                     # This was Evan's approach
     outf = open("out2.ascii", "w")
-    sigma_bins = width_bins/(m.sqrt(8.0*m.log(2.0)))    
+    sigma_bins = width_bins/(m.sqrt(8.0*m.log(2.0)))           #  FWHM^2 == 8*ln(2)*sigma^2
     for i in range(0,period_bins):
-#        prof[i] = (A/fac)*m.exp(-(i - offset_bins)**2/(2*sigma_bins**2)) 
         prof[i] = (A/fac)*m.exp(-(i - offset_bins)**2/(2*sigma_bins**2)) 
-if (option == 3):
+if (option == 3):                     # This was VG's approach
     outf = open("out3.ascii", "w")
-    sigma_bins = (A/fac)*width_bins/(m.sqrt(8.0*m.log(2.0)))    
+    sigma_bins = (A/fac)*width_bins/(m.sqrt(8.0*m.log(2.0)))
     for i in range(0,period_bins):
         prof[i] = A*m.exp(-(i - offset_bins)**2/(2*sigma_bins**2)) 
+if (option == 4):                     # This is the Vincent way, \int (g(t))^2 = 1 square-normalisation
+    outf = open("out4.ascii", "w")
+    sigma_bins = width_bins/(m.sqrt(8.0*m.log(2.0)))    
+    for i in range(0,period_bins):
+        prof[i] = (m.exp(-(i - offset_bins)**2/(2*sigma_bins**2)))
+    norm = (sum(prof**2))**0.5        # Normalise by the L^2 norm
+    prof /= norm                      # Normalise by the L^2 norm
+    prof *= m.sqrt(width_bins)        # Scale up the Gaussian rather than scaling down the boxcar filter
 
 print "Pulse Area: ",sum(prof)
 pulsar = np.hstack(prof for i in range(nperiods))
 time_series = height*pulsar + noise[0:pulsar.size]
 print height*pulsar[490:510]
-#print time_series[490:510]
+print height*pulsar[990:1010]
 #plt.plot(time_series)
 #plt.show()
 
